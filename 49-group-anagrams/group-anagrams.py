efbeb8db95c5bc289458eeb39266b6bbc528f61b
Sabbir Hossain <sabbir.cse.duet@gmail.com>
2026-07-23 10:27:56 +0600

    Time: 11 ms (82.84%) | Memory: 24.1 MB (9.10%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,3 +1,17 @@
+# Solution 1
+##from collections import defaultdict
+##
+##class Solution:
+##    def groupAnagram(self, strs):
+##        groups = defaultdict(list)
+##        for word in strs:
+##            key = "".join(sorted(word))
+##            groups[key].append(word)
+##
+##        return list(groups.values())
+##obj = Solution()
+##print(obj.groupAnagram(["eat","tea","tan","ate","nat","bat"]))
+
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
